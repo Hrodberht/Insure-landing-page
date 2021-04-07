0c5c19f90c7b6ca93a4b17693eb5cbbd019a67dd
Hrodberht <hrodberht.stefanescu@gmail.com>
2021-04-07 12:20:58 +0100

    Fixed toggle button

--- scripts/script.js
+++ scripts/script.js
@@ -3,35 +3,12 @@ const nav = document.getElementById('nav');
 const btn = document.getElementById('btn');
 const hamburger = document.getElementById('hamburger');
 const cross = document.getElementById('cross');
-var clicks = 1;
 
-function clickCount () {
-    clicks += 1;
-    console.log(clicks);
+function toggleNav () {
+    nav.classList.toggle('nav--show-nav');
+    header.classList.toggle('header--show-nav');
+    hamburger.classList.toggle('dropdown--hidden');
+    cross.classList.toggle('dropdown--hidden');
 }
 
-function showNav () {
-    header.classList.add('header--show-nav');
-    nav.style.display = 'block';
-    hamburger.classList.add('dropdown--hidden');
-    cross.classList.remove('dropdown--hidden');
-}
-
-function hideNav () {
-    header.classList.remove('header--show-nav');
-    nav.style.display = "none";
-    hamburger.classList.remove('dropdown--hidden');
-    cross.classList.add('dropdown--hidden');
-}
-
-function displayNav () {
-    if (clicks % 2 == 0) {
-        showNav();
-    } else {
-        hideNav();
-    }
-}
-
-btn.addEventListener('click', clickCount);
-btn.addEventListener('click', displayNav);
-
+btn.addEventListener('click', toggleNav);
